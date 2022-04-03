3e09cbbe6e8231bbddc2bc1105d13d172aa22e69
Alejandra Morales <you@example.com>
2022-04-03 18:45:10 -0400

    Agrego pagina de servicios y formulario

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(main);
+
+var contador = 1;
+
+function main(){
+	$('.menu_bar').click(function(){
+		// $('nav').toggle(); 
+
+		if(contador == 1){
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		}
+
+	});
+
+};
+
+function registrate() {
+	location.href = "registrate.html";
+}
+
+function servicios() {
+	location.href = "servicios.html";
+}
